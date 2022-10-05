95b054c54a468cff867732f98927b2ea9be07b74
VukhactruongBTH2 <truongvu0607@gmail.com>
2022-10-05 16:25:29 +0700

    vkt_5/10

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VukhactruongBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
