0a02d94b167f9ed417c5f1144a49418e06ccb120
truongvuhumg <112002156+truongvuhumg@users.noreply.github.com>
2022-10-12 23:16:01 +0700

    BTVN_0510

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+namespace VuKhacTruongBTH2.Models
+{
+    public class Student 
+    {
+        public string StudentID {get;set;}
+        public string StudentName {get ;set;}
+       
+        }
+
+}
